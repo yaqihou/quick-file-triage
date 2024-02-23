7f4c8f07bc7483ff36211b4c117c12429dc9b383
Yaqi Hou <yaqi.hou@yahoo.com>
2024-02-22 22:06:54 -0500

    Add commandline argument interface for entry script
    Signed-off-by: Yaqi Hou <yaqi.hou@yahoo.com>

--- src/run.py
+++ src/run.py
@@ -1,12 +1,21 @@
 
+from termcolor import colored
+
 from core.manager import Manager
 from core.enums import SortAttr, Category
+import argparse 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('folder', default='', nargs=1)
+parser.add_argument('-r', '--recursive', action='store_true')
+args = parser.parse_args()
+
+folder = args.folder[0]
+print('Changing current working directory to', colored(folder, 'green'))
 m= Manager(
-    folder='/media/yaqi/red2/',  # '/mnt/tmp2/',
-    recursive=True,
-    probe_on = True,
-    use_cache = {Category.AUDIO: False}
+    folder=folder,
+    recursive=args.recursive,
+    auto_probe = False,
 )
 # manager.organize(dry_run=False)
 # manager.summary()
